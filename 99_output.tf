5d988d4a5a3cea8cfbcd05efad7e91c057e009b4
penthium2 <penthium2@hotmail.fr>
2023-11-02 16:17:11 +0100

    new output

--- 99_output.tf
+++ 99_output.tf
@@ -1,4 +1,4 @@
 #############output : 
-output "Vm_ip" {
-  value = libvirt_domain.dynamic[*].network_interface.0.addresses
+output "VM_information" {
+  value = [libvirt_domain.dynamic[*].network_interface.0.hostname, libvirt_domain.dynamic[*].network_interface.0.addresses.0]
 }
