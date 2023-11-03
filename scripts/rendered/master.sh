e9c313e09ff8fb4970de30776d5059981fa7ec6d
penthium2 <penthium2@hotmail.fr>
2023-11-03 14:23:34 +0100

    modif gitignore

--- scripts/rendered/master.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-for vm in $(awk "/vm-[^0]/ {print $NF}" /etc/hosts) ; do
-    gluster peer probe "$vm"
-done
-sleep 5
-gluster volume create docker replica $(grep "vm-" /etc/hosts | wc -l) transport tcp $(awk '/vm-/ {print $1":/var/glusterfs/no-direct-write-here/docker/brick1"}' /etc/hosts) force
-gluster volume start docker
-sleep 5
