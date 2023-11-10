0cff829f5e23df10820625b559ea9532179aae4e
penthium2 <penthium2@hotmail.fr>
2023-11-10 12:12:11 +0100

    Finalisation Ansible avec up/down de nodes

--- 06_ansible.tf
+++ 06_ansible.tf
@@ -2,9 +2,21 @@ resource "local_file" "hosts_cfg" {
     content = templatefile("./ansible/inventory.tmpl",
         {
             swarm_hosts = libvirt_domain.dynamic[*].network_interface.0.addresses.0
+            replicas = var.number_vm
         }
     )
     filename = "./ansible/inventory.yml"
+    lifecycle {
+    replace_triggered_by = [
+      null_resource.always_run
+    ]
+    create_before_destroy = true
+  }
+}
+resource "null_resource" "always_run" {
+  triggers = {
+    timestamp = "${timestamp()}"
+  }
 }
 resource "null_resource" "ansible_provisioner" {
   depends_on = [
@@ -13,5 +25,11 @@ resource "null_resource" "ansible_provisioner" {
   provisioner "local-exec" {
     command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u root --private-key ../private_key.pem -i inventory.yml playbook.yml"
     working_dir = "${path.module}/ansible"
+  }
+  lifecycle {
+    replace_triggered_by = [
+      null_resource.always_run
+    ]
+    create_before_destroy = true
   }
 }
